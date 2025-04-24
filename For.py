3d97dd0217f50272426a993b9521bb0549c3eef1
Sparky21501 <esteban.rojas.fuentes215@gmail.com>
2025-04-23 21:36:58 -0400

    commit nuevo

---
+++ For.py
@@ -0,0 +1,42 @@
+# for i in range (1, 10):
+#   print("Esta es la tabla del", i)
+# for j in range(1, 10):
+#   print(i, "X", j, "=", i + j) 
+
+# for i in range (6):
+#  print("hola")
+  
+# for i in range (5):
+#  print(i+1)
+
+# cant=int(input("Ingrese la cantidad de repeticiones"))
+
+# for i in range (cant):
+#  print("repeticion", i+1)
+
+# nombre=input("ingrese su nombre") 
+# edad=input("ingrese su edad")
+# print("hola", nombre, "su edad es", edad)
+# print(f"Hola (nombre) su edad es (edad)")
+
+# num=int(input("ingrse un numero "))
+# for i in range(1,11 ):
+#  print(num,"x", i, "=", i*num)
+
+
+
+# for i in range (1,10):
+#   print("Esta es la tabla del", i)
+# for j in range(1,10):
+#  print(i,"X", j, "=",i*j)  
+
+# cant=int(input("ingrese la cantidad de nota"))
+# total=0
+ 
+# for i in range (cant):
+#   print("ingrese la nota", i+1)
+#   nota=float(input())
+#   total=total+nota
+# print(f"su promedio es"(prom))
+# prom=total/cant
+
---
+++ hola.py
@@ -0,0 +1,25 @@
+
+# #ingreso de variables para concatenar
+# print("ingrese su nombre")
+# nombre=input()1
+# print("ingrese su edad")
+# edad=input() 
+
+# # ejemplo de concatenacion
+# print("¡Hello",nombre, "y su edad es",edad)
+
+# #suma de 2 numeros
+# n1=int(input("ingrse un numero"))
+# n2=int(input("ingrse un segundo numero"))
+
+# print("el resultado de la multiplicacion es ",n1*n2)
+
+# # promedio de 3 numeros 
+
+# n1=int(input("ingrse un numero"))
+# n2=int(input("ingrse un segundo numero"))
+# n3=int(input("ingrse un tercer numero"))
+
+# resultado=((n1+n2+n3)/3)
+
+# print("el promedio final es de", resultado)
