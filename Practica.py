6bb5aceb5cb1c3b77b3ea9ff058d3a13aa3f295c
Sparky21501 <esteban.rojas.fuentes215@gmail.com>
2025-05-06 05:15:45 -0400

    Signo Zodiacal

--- Practica.py
+++ Practica.py
@@ -404,3 +404,34 @@
 # else:
 #     print("Es un empate")
 
+##-----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Pedir dia y mes de nacimiento y mostrar el signo zodiacal
+
+# dia = int(input("Ingrese el día de su nacimiento (1-31): "))
+# mes = int(input("Ingrese el mes de su nacimiento (1-12): "))
+
+# if mes == 3 and dia >= 21 or mes == 4 and dia <=19:
+#     print("Usted es Aries")
+# elif  mes == 4 and dia >20 or mes == 5 and dia <=20:
+#     print("Usted es Tauro")
+# elif  mes == 5 and dia >21 or mes == 6 and dia <=21:
+#     print("Usted es Geminis")
+# elif  mes == 6 and dia >22 or mes == 7 and dia <=21:
+#     print("Usted es Cancer")
+# elif  mes == 7 and dia >22 or mes == 8 and dia <=22:
+#     print("Usted es Leo")
+# elif  mes == 8 and dia >23 or mes == 9 and dia <=22:
+#     print("Usted es Virgo")
+# elif  mes == 9 and dia >23 or mes == 10 and dia <=22:
+#     print("Usted es Libra")
+# elif  mes == 10 and dia >23 or mes == 11 and dia <=22:
+#     print("Usted es Escorpio")
+# elif  mes == 11 and dia >23 or mes == 12 and dia <=22:
+#     print("Usted es Sagitario")
+# elif  mes == 12 and dia >23 or mes == 1 and dia <=21:
+#     print("Usted es Capricornio")
+# elif mes == 1 and dia >22 or mes == 2 and dia <=17:
+#     print("Usted es Acuario")
+# elif mes == 2 and dia >18 or mes == 3 and dia <=19:
+#     print("usted es Piscis")
