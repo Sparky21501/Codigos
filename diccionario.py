f1fac34166017298a3fb915212a172189e0d31d6
Sparky21501 <esteban.rojas.fuentes215@gmail.com>
2025-06-13 18:31:25 -0400

    diccionario

---
+++ diccionario.py
@@ -0,0 +1,83 @@
+# diccionario = conjunto de pares de datos
+
+# dic = {
+#     "nombre":"Mel broks",
+#     "numero": 964063677,
+#     "casado": True,
+#     123:98
+# }
+
+# print(dic)
+
+# for key,value in dic.items(): 
+#     print(key,value)
+
+
+# dic["ciudad"] = "talca"
+
+# for key, lol in dic.items():
+#     print(key,lol)
+
+# frutas = {
+#     "sandia" : 3000,
+#     "manzana" : 4000,
+#     "naranja" : 1500
+# }
+
+# print(frutas)
+# fruta = input("Ingrese una fruta : ")
+# precio = int(input("Ingrese el precio : "))
+# frutas[fruta]=precio
+# print(frutas)
+
+
+frutas = {
+    "Peras" : 600,
+    "Manzanas" : 350,
+    "Sandia" : 1500,
+    "Palta" : 2000
+}
+
+while True:
+    opc =int(input('''
+                   Seleccione una opcion
+                   1.- Ingresar fruta y precio
+                   2.- Actualizar precio
+                   3.- Borrar fruta y precio
+                   4.- mostrar todas las frutas y precios
+                   5.- comprar
+                   6.- salir
+                   '''))
+    
+    match opc:
+        case 1:
+            print(frutas)
+            fruta = input("Ingrese una fruta : ")
+            precio = int(input("Ingrese el precio : "))
+            frutas[fruta]=precio
+            print(frutas)
+        case 2:
+            for key, precio in frutas.items():
+                print(key,precio)
+            fruta = input("ingrese nombre de la fruta : ")
+            precio = int(input("Ingrese nuevo precio : "))
+            frutas[fruta]=precio
+
+
+        case 3:
+            for key, precio in frutas.items():
+                print(key,precio)
+            dell = input("Seleccione la fruta a borrar")
+            
+        case 4:
+            for key, precio in frutas.items():
+                print(key,precio)
+        case 5:
+            print("Que desea comprar")
+            for key, precio in frutas.items():
+                print(key,precio)
+        case 6:
+            print("Saliendo")
+            break
+        case _:
+            print("opcion invalida")
--- lista 2.py
+++ lista 2.py
@@ -63,43 +63,105 @@
 #             print("Opcion invalida")
 
 
-lista_productos = ["Tomate","Palta","Choripan"]
-precios = [1000, 900, 500]
-carrito = []
+# lista_productos = ["Tomate","Palta","Choripan"]
+# precios = [1000, 900, 500]
+# carrito = []
 
-while True:
-    opc = int(input('''Seleccione una opcion
-                    1.- Ingresar producto a la tienda
-                    2.- Comprar
-                    3.- Crear boleta
-                    4.- Salir
-                            '''))
-    match opc:
-        case 1:
-            producto = input("Ingrese producto a llevar : ")
-            lista_productos.append(producto)
-            print(f"Usted agrego {producto} a la tienda ")
-            productoprecio = int(input("Ingrese precio del producto : "))
-            precios.append(productoprecio)
-        case 2:
-            total = 0
-            print("Seleccione producto que se llevara")
-            for x in range(len(lista_productos)):
-                print(x + 1, ".-", lista_productos[x], "$",precios[x])
-            pro=int(input("Seleccione que quiere comprar : "))
-            carrito.append(pro-1)
-            print(f"Se agrego {pro} al carrito")
-            print(carrito)
-        case 3:
-            for i in carrito:
-                print(lista_productos[x], "-$",precios[x])
-                total = total + precios [x]
-            print("El Total de articulos es", len(carrito))
-            print("Su total neto es ", total)
-            print("Su total mas iva es ", total * 1.19 )
-        case 4:
-            print("Hasta luego gracias por su compra")
-            break
-        case _:
-            print("Error, opcion invalida")
-    
+# while True:
+#     opc = int(input('''Seleccione una opcion
+#                     1.- Ingresar producto a la tienda
+#                     2.- Comprar
+#                     3.- Crear boleta
+#                     4.- Salir
+#                             '''))
+#     match opc:
+#         case 1:
+#             producto = input("Ingrese producto a llevar : ")
+#             lista_productos.append(producto)
+#             print(f"Usted agrego {producto} a la tienda ")
+#             productoprecio = int(input("Ingrese precio del producto : "))
+#             precios.append(productoprecio)
+#         case 2:
+#             total = 0
+#             print("Seleccione producto que se llevara")
+#             for x in range(len(lista_productos)):
+#                 print(x + 1, ".-", lista_productos[x], "$",precios[x])
+#             pro=int(input("Seleccione que quiere comprar : "))
+#             carrito.append(pro-1)
+#             print(f"Se agrego {pro} al carrito")
+#             print(carrito)
+#         case 3:
+#             for i in carrito:
+#                 print(lista_productos[x], "-$",precios[x])
+#                 total = total + precios [x]
+#             print("El Total de articulos es", len(carrito))
+#             print("Su total neto es ", total)
+#             print("Su total mas iva es ", total * 1.19 )
+#         case 4:
+#             print("Hasta luego gracias por su compra")
+#             break
+#         case _:
+#             print("Error, opcion invalida")
+
+
+
+# notas = [7.0, 4.6, 4.9, 7.0, 6.9]
+
+# while True:
+#     try:
+#         print('''
+#             1.- Ingresar nota
+#             2.- Borrar nota
+#             3.- Mostrar notas
+#             4.- Sacar promedio, nota mayor y nota menor
+#             5.- Limpiar todas las notas
+#             6.- Salir''')
+#         try:
+#             op=int(input("Seleccione una opcion : "))
+#         except Exception:
+#             print("Solo numeros sin decimales")
+#         match op:
+#             case 1:
+#                 try:
+#                     nota = float(input("Ingrese una nota : "))
+#                     notas.append(nota)
+#                 except Exception:
+#                     print("Solo debe ingresar numeros con decimal EJ: 6.0, 7.0")
+#             case 2:
+#                 try:
+#                     for x, nota in enumerate(notas):
+#                         print(x+ 1,".-", nota)
+#                     eliminar = int(input("Que nota desea eliminar : ")) -1
+#                     notas.pop(eliminar)
+#                     print("Nota eliminada")
+#                 except Exception:
+#                     print("Debe de ingresar numero de la nota, no la nota")
+#             case 3:
+#                 print("Sus notas son")
+#                 print(notas)
+#             case 4:
+#                 suma = sum(notas)
+#                 total_notas = len(notas)
+#                 promedio = suma / total_notas
+#                 print(f"Su promedio es {promedio} " )
+#                 notas.sort()
+#                 print("La nota mayor es ", notas [-1])
+#                 print("La nota menor es ", notas [0])
+#             case 5:
+#                 opc = int(input('''
+#                     Desea eliminar todas las notas
+#                     1.- Si
+#                     2.- No
+#                                 '''))
+#                 if opc == 1:
+#                     print("Borrando notas")
+#                     notas.clear()
+#                 else:
+#                     print("Abortando...")
+#             case 6:
+#                 print("Saliendo....")
+#                 break   
+#             case _:
+#                 print("Error, Opcion invalida")
+#     except Exception:
+#         print("Solo debe ingresar numero")
