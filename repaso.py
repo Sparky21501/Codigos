e134bc2d9b648bac3c398a6293ff88839b16c0b4
Sparky21501 <esteban.rojas.fuentes215@gmail.com>
2025-06-26 08:24:55 -0400

    Garage repaso

--- repaso.py
+++ repaso.py
@@ -30,16 +30,22 @@ autos = {
 }
 
 
-
 def agregar():
-    print("-|-|-|-|-|-|-|-|-|-|-")
+    
+    print("***************************************************")
     print("Bienvenido a Garage Sparky's")
     agauto = input("Ingre Marca del vehiculo: ")
     agaño = int(input("Ingrese Año del vehiculo: "))
-    agpatente = input('''Ingrese pantenwte
+    agpatente = input('''Ingrese pantente
                     A considerar
                     tiene que tener 4 letras minusculas y 2 numeros
                     : ''')
+    if len(agpatente) != 6: # len(Nombre de variable agregada) ayuda a poner un limite de caracteres
+        print("Error")
+        print("Solo puede tener 6 caracteres maximo")
+        return
+    if not agpatente[4].isdigit():
+        print("Error los ultimos 2 caracteres no son numeros")
     agtipo = input('''Ingrese tipo de auto
                    -Electrico (E)
                    -Camioneta (C)
@@ -51,30 +57,96 @@ def agregar():
         print("Error")
         print("solo debe ser una Letra")
         return
-    ag_garage=max(autos.keys())+1
+    ag_garage=max(autos.keys())+1 # con max(nombre del diccionario.keys()) +1 podemos agregar un ID al diccionario original
     autos[ag_garage] ={
-        "Marca" :agauto,
+        "marca" :agauto,
         "año" : agaño,
         "patente": agpatente,
         "tipo" : agtipo}
     print("Auto Agregado al garage")
 
 
+
 def mostrar():
+    print("****************************")
+    print("Los siguientes autos estan en el garage")
     for ids, datos in autos.items():
-        print("****************************")
-        print("Los siguientes autos estan en el garage")
         print(f"Auto n°{ids}")
         for clave, valor in datos.items():
             print(f"{clave} : {valor}")
 
 
 def actualizar():
-    for ids, datos in autos.items():
         print("****************************")
         print("Los siguientes autos estan en el garage")
-        print(f"Auto n°{ids}")
-        for clave, valor in datos.items():
-            print(f"{clave} : {valor}")
+        for ids, datos in autos.items():
+            print(f"Auto n°{ids}")
+            for clave, valor in datos.items():
+                print(f"{clave} : {valor}")
+        actautos = int(input("Ingrese id de auto a actualizar: "))
+        marca = input("Ingrese Marca del auto: ")
+        año = int(input("Ingrese año del auto"))
+        patente = input("Ingrese patente del auto: ")
+        tipo = input("Ingrese tipo del auto: ")
+        if len(tipo) != 1:
+            print("Error")
+            print("solo debe ser una Letra")
+            return
+        else:
+            print("****************")
+            print("Auto actualizado")
+            print("****************")
+        autos[actautos] = {"marca" : marca, "año": año, "patente" : patente, "tipo" : tipo}
 
-actualizar()
+def borrar():
+    try:
+        for ids, datos in autos.items():
+            print(f"Auto n°{ids}")
+            for clave, valor in datos.items():
+                print(f"{clave} : {valor}")
+        eliminar = int(input("Seleccione ID de auto a eliminar"))
+        del autos[eliminar]
+        print("Auto eliminado")
+    except Exception:
+        print("Error :( )")
+        print("Solo debe seleccionar numeros enteros de los que aparecen, en ese caso el ID del auto)") 
+
+def estadisticas():
+    print("*************************************")
+    ultimo_id = max(autos) # max(nombre del diccionario) busca el id mas grande del diccionario 
+    ultimo_auto = autos[ultimo_id]
+    print(f"El ultimo auto ingresado fue {ultimo_auto['marca']} del año {ultimo_auto['año']}, con patente {ultimo_auto['patente']}, tipo {ultimo_auto['tipo']} ")
+    print(f"El total de autos en el Garage son: {len(autos)}") # con {len(nombre del diccionario)} se puede ver el total de cosas que hay en el diccionario
+    print("*************************************")
+
+while True: 
+    try:
+        opc = int(input('''Seleccione una opcion
+                        1.- Ingresar Vehiculo
+                        2.- Mostrar Vehiculos
+                        3.- Actualizar Vehiculo
+                        4.- Borrar Vehiculo
+                        5.- Mostar estadisticas: ultimo vehiculo ingresado, y total en garage
+                        6.- Salir
+                        :::::::::::::  '''))
+        
+        match opc:
+            case 1:
+                agregar()
+            case 2: 
+                mostrar()
+            case 3: 
+                actualizar()
+            case 4:
+                borrar()
+            case 5:
+                estadisticas ()
+            case 6:
+                print("Saliendo...")
+                break
+            case _:
+                print("Opcion invalida")
+    except Exception:
+        print("Error :( )")
+        print("Solo debe seleccionar numeros enteros de los que aparecen)")            
+            
